c021355b6ae2a0b447c10f2615c0bf88768982a9
murilo <muriloamancio0@gmail.com>
2023-02-27 22:19:59 -0300

    seed

---
+++ teste_upd8/database/seeders/ClienteSeed.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClienteSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('clientes')->insert([
+            'nome' => 'Helena Karol Pinheiro Maia',
+            'email' => 'helenamaia@hotmail.com',
+            'endereco' => 'rua deodoro da fonseca nº987',
+            'sexo' => 'Feminino',
+            'estado' => 'Paraná',
+            'cidade' => 'Pontal do Paraná',
+            'data_nascimento' => '09_03_2004'
+        ]);
+
+    }
+}
